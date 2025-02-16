7ab5918151e81d5d90c5ebc76cdb6d2fa5de2b8e
Jake Stambaugh <jake.stambaugh@gmail.com>
2025-02-16 18:16:32 -0500

    Add basic clap parsing for files and the design notes

---
+++ DESIGN.md
@@ -0,0 +1,19 @@
+* No module system
+
+* No multi-file support
+
+* No build system
+
+* Test framework integrated into the file like Rust
+
+* Whitespace sensitive? Easier to write down when programming forward
+
+* Std collections library with easy runtime understanding
+
+* Easy record and tuple types with destructuring
+
+* No tail recursion (stack overflow is a useful signal)
+
+* Static typing
+
+* Pointers (or something to let people show off ptr math / 2 ptr windowing)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(value_parser, value_name = "SCRIPT")]
+    path: PathBuf,
 }
+
+fn main() {
+    let args = Args::parse();
+
+    println!("{:?}", args.path);
+
+    if let source_code = read_source_code(&args.path) {
+    } else {
+        eprintln!("Failed to read source code from {:?}", args.path)
+    }
+}
+
+enum FellowError {
+    CannotReadFile(String),
+}
+
+fn read_source_code(path: &PathBuf) -> Result<String, FellowError> {}

e5524e96803f7e5797179d0acd387cb7c496d778
Jake Stambaugh <jake.stambaugh@gmail.com>
2025-02-16 20:23:58 -0500

    Basic interpreter
    It just spits out the last line of the input file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::error::Error;
+use std::fmt::{self, Display};
 use std::path::PathBuf;
 
 use clap::Parser;
@@ -14,14 +16,63 @@ fn main() {
 
     println!("{:?}", args.path);
 
-    if let source_code = read_source_code(&args.path) {
-    } else {
-        eprintln!("Failed to read source code from {:?}", args.path)
+    match read_source_code(&args.path) {
+        Ok(contents) => match interpret(contents) {
+            Ok(value) => println!("{}", value),
+            Err(err) => eprintln!("Error {:?}", err),
+        },
+        Err(err) => eprintln!("Failed to read source code from {:?}", err),
     }
 }
 
+#[derive(Debug)]
 enum FellowError {
-    CannotReadFile(String),
+    CannotReadFile,
+    InterpreterError,
 }
 
-fn read_source_code(path: &PathBuf) -> Result<String, FellowError> {}
+impl Error for FellowError {}
+
+impl fmt::Display for FellowError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Fellow error occurred")
+    }
+}
+
+fn read_source_code(path: &PathBuf) -> Result<String, FellowError> {
+    match std::fs::read_to_string(path) {
+        Ok(contents) => Ok(contents),
+        Err(_err) => Err(FellowError::CannotReadFile),
+    }
+}
+
+enum FellowValue {
+    Int(i64),
+    String(String),
+}
+
+impl Display for FellowValue {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Int(i) => write!(f, "{:?}", i),
+            Self::String(s) => write!(f, "{}", s),
+        }
+    }
+}
+
+// Pipeline for our interpreter
+fn interpret(source_code: String) -> Result<FellowValue, FellowError> {
+    let tokens = source_code.split_whitespace();
+    match tokens.last() {
+        Some(v) => Ok(parse_token(v)),
+        None => Err(FellowError::InterpreterError),
+    }
+}
+
+fn parse_token(token: &str) -> FellowValue {
+    if let Ok(i) = token.parse::<i64>() {
+        FellowValue::Int(i)
+    } else {
+        FellowValue::String(token.to_string())
+    }
+}
